07227d94bd261f0bd064cd11da67cb70c088ba75
Juan Carlos <juanki97r@gmail.com>
2025-12-16 12:45:42 +0100

    terminando restriccion de parametros

--- src/main/java/modelo/Animal.java
+++ src/main/java/modelo/Animal.java
@@ -1,6 +1,26 @@
 package modelo;
 
+import java.time.LocalDate;
+
 public class Animal {
     
+    private LocalDate fechaNacimiento;
+    private String nombre;
+    private final String[] TIPO = {"gato","perro","cobaya", "lagarto", "periquito" };
+    private String tipo;
+    private int peso;
+    private String[] ESTADO = {"comiendo","durmiento","reposando","jugando"};
+    private String estado;
+
+    //constructor por defecto
+    public Animal (){
+    }
+
+    public Animal (LocalDate fechaNac,String nombre,int peso, String tipo ){
+        if((fechaNac==null && fechaNac.isBefore(LocalDate.of(2000, 1, 1)) && fechaNac.isAfter(LocalDate.now())) )
+
+        this.tipo="reposo";
+    }
+
     
 }
